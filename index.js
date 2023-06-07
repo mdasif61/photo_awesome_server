e08058a7d5a8e1e850ba591508b064aad72fc62a
ikbal hossain asif <asifikbalgfh312@gamil.com>
2023-06-07 19:38:29 +0600

    classesCollection create

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
-const express=require('express')
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+const cors = require('cors');
+require('dotenv').config();
+const jwt = require('jsonwebtoken');
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Photo Awesome is Running')
+})
+
+// photo-awesome
+// zaVPqYskgkDLiZet
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://photo-awesome:zaVPqYskgkDLiZet@cluster0.kuomool.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+
+        await client.connect();
+
+        const classesCollection=client.db('photo_awesome').collection('classes')
+
+        app.get('/classes',async(req,res)=>{
+            const result=await classesCollection.find().toArray();
+            res.send(result)
+        })
+
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+app.listen(port, () => {
+    console.log('server running port : ', port)
+})
